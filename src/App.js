113a7216574f68c36ff474e5e6fa0e38e5aa002b
CKMolinga <ck.molinga@gmail.com>
2022-08-22 05:36:02 +0200

    pull changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 //* import components
-import Setup from "./tutorial/3-conditional-rendering/setup/3-show-hide";
+import Setup from "./tutorial/4-forms/setup/1-controlled-inputs";
 function App() {
   return (
     <div className="container">
